5a5cd1e6d523738adbadbfda0285d78087f019a6
Jayabharathy-R <94303082+Jayabharathy-R@users.noreply.github.com>
2022-07-27 09:09:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express=require('express');
 const mongo=require("./shared");
-const hallBookingRouter=require('./router/rooms')
+const hallBookingRouter=require('./router/rooms');
+//const dotenv=require('dotenv');
 
+//dotenv.config();
+var app=express();
 
-var app=express()
 
 app.use(express.json());
 
 mongo.connect();
+
+
+
 app.use('/',(req,res,next)=>{
     next();
 })
 
 app.use('/hallBooking',hallBookingRouter);
 
-app.listen('3000');
+app.listen('3003');
--- shared.js
+++ shared.js
@@ -1,10 +1,25 @@
+// const {MongoClient}=require('mongodb');
+
+// module.exports={
+//     slectedDb:{},
+//     async connect(){
+//         try{
+//            const client=await MongoClient.connect('mongodb+srv://Jayabharathy:Jai151991@cluster0.tzkph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+//          this.slectedDb=client.db("hallBooking");
+//          console.log(this.slectedDb);
+//         }
+//         catch(err){
+//             console.log(err)
+//         }
+//     }
+// }
 const {MongoClient}=require('mongodb');
 
 module.exports={
     slectedDb:{},
     async connect(){
         try{
-           const client=await MongoClient.connect("mongodb://localhost:27017")
+           const client=await MongoClient.connect('mongodb+srv://Jayabharathy:Jai151991@cluster0.tzkph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
          this.slectedDb=client.db("hallBooking");
          console.log(this.slectedDb);
         }
